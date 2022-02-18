91d89bc85ccb408622a0a1b3861d659513b19ff6
Chue Chang <chuechang1996@outlook.com>
2022-02-18 02:22:36 -0600

    fix auth.js and add more to helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,13 @@
 module.exports = {
   // Formats date to display correctly when passed into handlebars
-  format_date: date => {
+  setDate: date => {
+    dateString = JSON.stringify(date);
+    noQuotes = dateString.split('"');
+    splitString = noQuotes[1].split('T');
+    secondSplit = splitString[1].split('.');
+    return (`${splitString[0]}, ${secondSplit[0]}`);
+  },
+  formatDate: date => {
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
   }
 };
