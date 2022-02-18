21e80217f042f0b9f1a30c5d3ec9fd11fc679b5f
Chue Chang <chuechang1996@outlook.com>
2022-02-18 01:17:44 -0600

    server js, cooment routes and index routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,1 +1,22 @@
+const router = require('express').Router();
+const { Comment, Post, User } = require('../models');
 
+router.get('/', async (req, res) => {
+    try {
+      const commentData = await Comment.findAll();
+      res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+    try {
+      const commentData = await Comment.create({
+        body: req.body.body,
+      });
+      res.status(200).json(commentData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,10 @@
+const router = require('express').Router();
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes');
+const apiRoutes = require('./api');
 
+router.use('/api', apiRoutes);
+router.use('/home', homeRoutes);
+router.use('/dash', dashboardRoutes);
+
+module.exports = router;
