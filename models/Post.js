054144a0961b815a20073e43ccb4630de49d5e40
Chue Chang <chuechang1996@outlook.com>
2022-02-18 22:02:03 -0600

    improve display

--- models/Post.js
+++ models/Post.js
@@ -10,6 +10,27 @@ Post.init(
   },
   {
     sequelize
+  },
+  {
+    // When adding hooks via the init() method, they go below
+    hooks: {
+      // Use the beforeCreate hook to work with data before a new instance is created
+      beforeCreate: async (newTitle) => {
+        // In this case, we are taking the user's title address, and making all letters lower case before adding it to the database.
+        newTitle.title = await newTitle.title.toUpperCase();
+        return newTitle;
+      },
+      // Here, we use the beforeUpdate hook to make all of the characters lower case in an updated title address, before updating the database.
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.title = await updatedUserData.title.toUpperCase();
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
   }
 );
 
